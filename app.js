4a3fbe4520bb35a44c36603762f618eb97bb144c
josh nickson <jnickson1506@gmail.com>
2018-09-11 14:10:44 +0100

    code cleanup

--- app.js
+++ app.js
@@ -2,22 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient
-
 const app = express();
 
-// Body parser middleware - will handle parsing json content - not sure exactly what this does!!
+// ____ Setup required middleware ____ 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
-// set static path
-// app.use(express.static(path.join(__dirname, 'public'))) -----no idea if this is necessary!
-
-// middleware to set the view engine - setting it to use ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Establish database connection 
-// Change mongo database path for testing environment
+// ____ Establish database connection ____ 
 MongoClient.connect('mongodb://makers1:makers1@ds251332.mlab.com:51332/makersbnb', { useNewUrlParser: true }, (err, client) => {
   if (err) return console.log(err)
   db = client.db('makersbnb') 
@@ -26,39 +19,33 @@ MongoClient.connect('mongodb://makers1:makers1@ds251332.mlab.com:51332/makersbnb
   })
 })
 
-//__________ROUTES____________
-
+//__________ R O U T E S __________
 app.get('/', function (req, res) {    // One-line equivalent syntax: (req, res) => res.send('Hello World!'))
- res.render('index')
+  res.render('index')
 });
 
 app.get('/homepage', function (req, res) {
   db.collection('properties').find().toArray((err, result) => {
     if (err) return console.log(err)
     res.render('homepage', {properties: result});
-    console.log(result)
   })
 });
 
 app.post('/homepage/add', function(req, res) {
-    db.collection('properties').save(req.body, (err, result) => {
-      if (err) return console.log(err)
-      console.log(result)
-      res.redirect('/homepage')
-    })
-  });
-
-app.get('/makeBooking', function (req, res) {
- res.render('makeBooking');
+  db.collection('properties').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    res.redirect('/homepage')
+  })
 });
 
 app.get('/createListing', function (req, res) {
- // res.send('This is another string returned from the /newListing route')
- res.render('createListing');
+  res.render('createListing');
 });
 
-// throwing in a route to a login form if we need one later
+app.get('/makeBooking', function (req, res) {
+  res.render('makeBooking');
+});
+
 app.get('/loginForm', function (req, res) {
   res.render('loginForm');
 });
-
