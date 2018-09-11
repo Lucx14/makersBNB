c11ecb7e41fa5191b07c0eafccf19aa03dc02b4f
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-11 16:15:32 +0100

    three feature tests working

--- spec/features/viewAllListingsSpec.js
+++ spec/features/viewAllListingsSpec.js
@@ -10,23 +10,32 @@ describe('View all listings features', function() {
     const browser = new Browser()
   // })
 
-describe('/', function() {
-  it('responds with Hello World!', function () {
-    browser.visit('/', function() {
-      console.log(browser.location.href)
-      browser.assert.success();
+  describe('/', function() {
+    it('gets there and responds', function () {
+      browser.visit('/', function() {
+        console.log(browser.location.href)
+        browser.assert.success();
+      })
     })
-    // expect(page).to have_content 'Hello World!'
-    // page.should have_content('Hello World!')
-
+    it('displays Welcome to MakersBNB', function() {
+      browser.visit('/', function() {
+        console.log(browser.location.href)
+        browser.assert.text('Welcome to MakersBNB');
+      })
+    })
   })
-})
-})
 
-// describe('Homepage', function() {
-//   it('displays list of all properties', function () {
-//     visit('/homepage')
-//     expect(page).to have_content('')
-//     // page.should have_content 'title'
-//   })
-// })
+  describe('/homepage', function() {
+    it('displays a listed property', function() {
+      browser.visit('/homepage', function () {
+      browser.assert.text('London');
+      })
+    })
+  })
+
+  describe('/homepage', function() {
+    it('takes you to /homepage/add')
+  })
+
+
+});
