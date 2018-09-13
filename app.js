9eb38f3b6684553352db2bf3f6614cb881350ba4
josh nickson <jnickson1506@gmail.com>
2018-09-13 10:43:29 +0100

    add authenticate input against database section from docs and not breaking

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ const session = require('express-session')
 //____Authentication with Mongoose___
 const mongoose = require('mongoose')
 
+// _______USER SCHEMA _______
 var UserSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true, trim: true },
   password: { type: String, required: true }
 });
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
 
 UserSchema.pre('save', function (next) {
   var user = this;
@@ -25,8 +29,26 @@ UserSchema.pre('save', function (next) {
   })
 });
 
-var User = mongoose.model('User', UserSchema);
-module.exports = User;
+//_______Authenticate input against database_______
+UserSchema.statics.authenticate = function (email, password, callback) {
+  User.findOne({ email: email })
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
+    });
+}
 
 // _______ MIDDLEWARE _______
 app.use(bodyParser.json());
